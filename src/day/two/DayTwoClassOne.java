91b8f512be6965ae4666fcf1c280743e82b77b96
username <email address>
2022-06-23 07:32:11 -0700

    project renamed and class moved

---
+++ src/day/two/DayTwoClassOne.java
@@ -0,0 +1,11 @@
+package day.two;
+
+public class DayTwoClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git branch <name> = creates the a new brance in git-hub");
+
+	}
+
+}
